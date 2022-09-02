b040d3b9ef452c85e9bbbd96048206e2f3f53722
Eddy-123 <adegnandjoueddy12@gmail.com>
2022-09-02 10:13:20 +0100

    React Routintiing

---
+++ frontend/src/components/CreateRoomPage.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class CreateRoomPage extends Component {
+  render() {
+    return (
+      <div>CreateRoomPage</div>
+    )
+  }
+}
---
+++ frontend/src/components/RoomJoinPage.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class RoomJoinPage extends Component {
+  render() {
+    return (
+      <div>RoomJoinPage</div>
+    )
+  }
+}
